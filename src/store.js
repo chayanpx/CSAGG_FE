6fe824850141455c10e99974a96947fc1c28681a
Chulalak U <chayanpx@gmail.com>
2020-02-16 15:21:07 +0700

    deploy

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const user = writable({
-    username:'',
-})
-
-export const postID = writable(null)
-// อยากให้ค่าเริ่มต้นเป็น 'Login'
-export const pages = writable('login') 
